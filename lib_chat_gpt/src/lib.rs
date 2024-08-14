c6e3dffaf95b1faf90842d1c4e9817b2fc914a81
satelight <crazy_soci80@yahoo.co.jp>
2024-08-15 02:22:10 +0900

    コメント追加

--- lib_chat_gpt/src/lib.rs
+++ lib_chat_gpt/src/lib.rs
@@ -1,12 +1,23 @@
 //! 会話の履歴データはAPIに保存しているわけでない。
 //! こちらで会話の履歴を保存する必要がある。
+//!
+//! こんな感じでデータを送れば履歴おくることができる。
+//! システムに答え方の形式やロールの要求する際は"system"　←これについてはサーバー側で固定している。
+//! 質問する側:"user"
+//! chatGPTの答えた内容は"assistant"
+//! [
+//!     {"role": "system", "content": "You are a helpful assistant."},
+//!     {"role": "user", "content": "こんにちは、天気はどうですか？"},
+//!     {"role": "assistant", "content": "こんにちは！今日は晴れています。"},
+//!     {"role": "user", "content": "明日の天気は？"}
+//! ]
 
 use anyhow::Ok;
 use serde::{Deserialize, Serialize};
 
 pub const OPENAI_API_URL: &str = "https://api.openai.com/v1/chat/completions";
 
-/// マークダウン形式で固定。
+/// 答える形式はマークダウン形式で固定。
 pub const AI_RESPONSE_ROLE: &str =
     "あなたは優秀なAIアシスタントです。返答はmarkdown形式でお願いします";
 
@@ -22,6 +33,30 @@ pub struct Message {
     content: String,
 }
 
+/// chatGPTからのデータ形式は以下のようになる。
+/// 必要なデータは[choices]部分のみ
+/// {
+///   "id": "chatcmpl-123",
+///   "object": "chat.completion",
+///   "created": 1677652288,
+///   "model": "gpt-4o-mini",
+///   "system_fingerprint": "fp_44709d6fcb",
+///   "choices": [{
+///      "index": 0,
+///      "message": {
+///        "role": "assistant",
+///        "content": "\n\nHello there, how may I assist you today?",
+///      },
+///      "logprobs": null,
+///      "finish_reason": "stop"
+///   }],
+///   "usage": {
+///      "prompt_tokens": 9,
+///      "completion_tokens": 12,
+///      "total_tokens": 21
+///   }
+/// }
+///
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct ChatGptResponse {
     choices: Vec<Choice>,
@@ -43,26 +78,29 @@ pub async fn response_from_chat_gpt(
     api_key: &str,
 ) -> anyhow::Result<String> {
     // リクエスト用メッセージ作成。
+    let mut msgs = vec![];
+
+    // 最初にAIに返答モデルを設定する。
     let ai_role_model = Message {
         role: "system".to_string(),
         content: String::from(AI_RESPONSE_ROLE),
     };
-    let mut msgs = vec![];
     msgs.push(ai_role_model);
-    println!("{:?}", request_messages);
+
+    // requestメッセージを追加。
     for resqest_message in request_messages {
         msgs.push(resqest_message);
     }
-
     println!("{:?}", msgs);
+
     // openAIのAPIにポストリクエストクライエント作成
-    let client = reqwest::Client::new();
     let request = ChatGptRequest {
         model: "gpt-3.5-turbo".to_string(),
         messages: msgs,
     };
 
     // openAIのAPIにリクエストを送る
+    let client = reqwest::Client::new();
     let res = client
         .post(OPENAI_API_URL)
         .bearer_auth(api_key)
@@ -73,7 +111,7 @@ pub async fn response_from_chat_gpt(
     //
     if res.status().is_success() {
         let response: ChatGptResponse = res.json().await.expect("Failed to parse response");
-        println!("{:?}", response.choices[0]);
+        println!("ChatGPTからの返答:{:?}", response);
         return Ok(response.choices[0].message.content.clone());
     } else {
         print!("NG");
